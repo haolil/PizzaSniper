d694682e05800fcb0e96d88af7023549963820ff
Zephyrus Mckinnon <zephyrus.mck@gmail.com>
2021-11-16 18:33:46 -0500

    Main Menu UI
    Added Main menu UI with button connections. Still need to add sound controller settings.

---
+++ Assets/UI/Sprites/Cursor/CursorRotate.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorRotate : MonoBehaviour
+{
+    public Texture2D[] cursorTextureArray;
+    public int frameCount;
+    public float frameRate;
+
+    private int currentFrame;
+    private float frameTimer;
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorTextureArray[0], new Vector2(50, 50), CursorMode.Auto);
+    }
+
+    void Update()
+    {
+        frameTimer -= Time.deltaTime;
+        if(frameTimer <= 0f)
+        {
+            frameTimer += frameRate;
+            currentFrame = (currentFrame + 1) % frameCount;
+            Cursor.SetCursor(cursorTextureArray[currentFrame], new Vector2(50, 50), CursorMode.Auto);
+        }
+    }
+}
