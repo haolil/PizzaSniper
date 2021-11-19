3fbc19e3237fc39cd4487da609a48122345d5e41
Zephyrus Mckinnon <zephyrus.mck@gmail.com>
2021-11-18 22:06:52 -0500

    Updated tracker

--- Assets/Scoring/ScoreTracking.cs
+++ Assets/Scoring/ScoreTracking.cs
@@ -1,30 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreTracking : MonoBehaviour
 {
+    public float hit = 0;
+    public float miss = 0;
+    public float total;
     public float accuracy = 100;
-
+    public Text accuracyValue, killValue;
+    GameObject[] pineapplesTotalAtStart = new GameObject[0];
+    GameObject[] pineapplesTotalAtEnd = new GameObject[0];
 
     void Start()
     {
-        
+        //pineapplesTotalAtStart = //get total enemies based on AI set up array
     }
-
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            //hit
+            hit++;
+            //kill
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
+            miss++;
             //miss
         }
 
-        Debug.Log("");
+        //final
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            CalculateAccuracy();
+            CalculateKills();
+            accuracyValue.text = accuracy.ToString() + "%";
+        }
+    }
+
+    public float CalculateAccuracy()
+    {
+        total = hit + miss;
+        accuracy = (hit / total) * 100;
+        accuracy = Mathf.Clamp(accuracy, 0, 100);
+        accuracy = Mathf.Round(accuracy);
+        return accuracy;
+    }
+
+    public void CalculateKills()
+    {
+        //pineapplesTotalAtEnd = //get total enemies based on AI set up array at the end of timer
+        killValue.text = pineapplesTotalAtEnd.Length.ToString() + "/" + pineapplesTotalAtStart.Length.ToString();
     }
 }
