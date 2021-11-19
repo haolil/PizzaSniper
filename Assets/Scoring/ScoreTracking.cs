23f64f5df57ca6f0b97c50a121ccafac567e5bc6
Zephyrus Mckinnon <zephyrus.mck@gmail.com>
2021-11-19 00:43:12 -0500

    Update ScoreTracking.cs

--- Assets/Scoring/ScoreTracking.cs
+++ Assets/Scoring/ScoreTracking.cs
@@ -9,7 +9,7 @@ public class ScoreTracking : MonoBehaviour
     public float miss = 0;
     public float total;
     public float accuracy = 100;
-    public Text accuracyValue, killValue;
+    public Text accuracyValue, killValue, timeValue, rankValue;
     GameObject[] pineapplesTotalAtStart = new GameObject[0];
     GameObject[] pineapplesTotalAtEnd = new GameObject[0];
 
@@ -54,5 +54,20 @@ public class ScoreTracking : MonoBehaviour
     {
         //pineapplesTotalAtEnd = //get total enemies based on AI set up array at the end of timer
         killValue.text = pineapplesTotalAtEnd.Length.ToString() + "/" + pineapplesTotalAtStart.Length.ToString();
+    }
+
+    public void CalculateTime()
+    {
+        //timeValue.text = //timeremaining.ToSring();
+    }
+
+    public void CalculateRank()
+    {
+        int unknownValue = 1;
+        switch (unknownValue)
+        {
+            case 0: rankValue.text = "D";
+                break;
+        }
     }
 }
