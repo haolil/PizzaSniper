cd18606057e2a09a44cca80a733cd81c113e8b3c
Zephyrus Mckinnon <zephyrus.mck@gmail.com>
2021-11-20 15:37:52 -0500

    Main menu updates
    Updated the main menu to level change to the correct levels and added the different gun and scope changes to the level. Still need level images to polish.

---
+++ Assets/UI/Scopes/ScopeController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class ScopeController : MonoBehaviour
+{
+    public int currentScope;
+
+    public void ScopeOne()
+    {
+        currentScope = 0;
+    }
+
+    public void ScopeTwo()
+    {
+        currentScope = 1;
+    }
+
+    public void ScopeThree()
+    {
+        currentScope = 2;
+    }
+
+    public void ScopeFour()
+    {
+        currentScope = 3;
+    }
+
+    public void ScopeFive()
+    {
+        currentScope = 4;
+    }
+
+    public void ScopeSix()
+    {
+        currentScope = 5;
+    }
+    public void ScopeSeven()
+    {
+        currentScope = 6;
+    }
+    public void ScopeEight()
+    {
+        currentScope = 7;
+    }
+}
