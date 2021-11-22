54b13a75b1b9f7229ba9d6bea6f3bfb8e234440c
Zephyrus Mckinnon <zephyrus.mck@gmail.com>
2021-11-21 22:58:39 -0500

    linked gun choice to fire rate

--- Assets/UI/CanvasPrefabs/UIResultGunDisplay.cs
+++ Assets/UI/CanvasPrefabs/UIResultGunDisplay.cs
@@ -6,6 +6,7 @@ public class UIResultGunDisplay : MonoBehaviour
     public Image gunUI;
     public Sprite gun1, gun2, gun3, gun4, gun5, gun6;
     GunController gunController;
+    Snipe snipe;
 
     private void Start()
     {
@@ -15,21 +16,27 @@ public class UIResultGunDisplay : MonoBehaviour
         {
             case 0:
                 gunUI.sprite = gun1;
+                snipe.fireRate = 5;
                 break;
             case 1:
                 gunUI.sprite = gun2;
+                snipe.fireRate = 5;
                 break;
             case 2:
                 gunUI.sprite = gun3;
+                snipe.fireRate = 5;
                 break;
             case 3:
                 gunUI.sprite = gun4;
+                snipe.fireRate = 5;
                 break;
             case 4:
                 gunUI.sprite = gun5;
+                snipe.fireRate = 5;
                 break;
             case 5:
                 gunUI.sprite = gun6;
+                snipe.fireRate = 5;
                 break;
         }
     }
