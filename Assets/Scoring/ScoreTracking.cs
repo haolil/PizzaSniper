48c53ba0707cb00c7cef56e1483efea8bc9a5cf2
Zephyrus Mckinnon <zephyrus.mck@gmail.com>
2021-11-22 22:46:17 -0500

    Added more AI to level 1

--- Assets/Scoring/ScoreTracking.cs
+++ Assets/Scoring/ScoreTracking.cs
@@ -15,7 +15,7 @@ public class ScoreTracking : MonoBehaviour
     public float totalKills;
     public float rankPercentage;
     Timer timer;
-    bool canKey;
+    bool canKey, rankCalulated;
     AIManager aiManager;
     Snipe snipe;
 
@@ -27,6 +27,7 @@ public class ScoreTracking : MonoBehaviour
         pineapplesTotalAtEnd = pineappleTotalAtStart;
         timer = FindObjectOfType<Timer>();
         snipe = FindObjectOfType<Snipe>();
+        rankCalulated = false;
     }
 
     void Update()
@@ -43,7 +44,6 @@ public class ScoreTracking : MonoBehaviour
 
     public void FinalResults()
     {
-        pineapplesTotalAtEnd = hit;
         CalculateAccuracy();
         CalculateKills();
         accuracyValue.text = accuracy.ToString() + "%";
@@ -62,7 +62,7 @@ public class ScoreTracking : MonoBehaviour
 
     public void CalculateKills()
     {
-        killValue.text = pineapplesTotalAtEnd.ToString() + "/" + pineappleTotalAtStart.ToString();
+        killValue.text = hit.ToString() + "/" + pineappleTotalAtStart.ToString();
     }
 
     public void CalculateTime()
@@ -72,34 +72,39 @@ public class ScoreTracking : MonoBehaviour
 
     public void CalculateRank()
     {
-        totalKills = pineappleTotalAtStart - pineapplesTotalAtEnd;
-        rankPercentage = (totalKills/ pineappleTotalAtStart) * 100;
-        rankPercentage = Mathf.Clamp(rankPercentage, 0, 100);
-        rankPercentage = Mathf.Round(rankPercentage);
-        float finalTotal = (rankPercentage + accuracy) * 0.5f;
-        finalTotal = Mathf.Clamp(finalTotal, 0, 100);
-        finalTotal = Mathf.Round(finalTotal);
-
-        switch (finalTotal)
+        if (!rankCalulated)
         {
-            case float n when n >= 100:
-                rankValue.text = "SSS";
-                break;
-            case float n when n >= 90:
-                rankValue.text = "S";
-                break;
-            case float n when n >= 70:
-                rankValue.text = "A";
-                break;
-            case float n when n >= 50:
-                rankValue.text = "B";
-                break;
-            case float n when n >= 20:
-                rankValue.text = "C";
-                break;
-            case float n when n >= 0:
-                rankValue.text = "D";
-                break;
+            pineapplesTotalAtEnd = pineapplesTotalAtEnd - hit;
+            totalKills = pineappleTotalAtStart - pineapplesTotalAtEnd;
+            rankPercentage = (totalKills / pineappleTotalAtStart) * 100;
+            rankPercentage = Mathf.Clamp(rankPercentage, 0, 100);
+            rankPercentage = Mathf.Round(rankPercentage);
+            float finalTotal = (rankPercentage + accuracy) * 0.5f;
+            finalTotal = Mathf.Clamp(finalTotal, 0, 100);
+            finalTotal = Mathf.Round(finalTotal);
+            rankCalulated = true;
+
+            switch (finalTotal)
+            {
+                case float n when n >= 100:
+                    rankValue.text = "SSS";
+                    break;
+                case float n when n >= 90:
+                    rankValue.text = "S";
+                    break;
+                case float n when n >= 70:
+                    rankValue.text = "A";
+                    break;
+                case float n when n >= 50:
+                    rankValue.text = "B";
+                    break;
+                case float n when n >= 20:
+                    rankValue.text = "C";
+                    break;
+                case float n when n >= 0:
+                    rankValue.text = "D";
+                    break;
+            }
         }
     }
 }
