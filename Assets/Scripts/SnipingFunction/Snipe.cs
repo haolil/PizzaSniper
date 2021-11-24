3c80e81d73ba48db7789dc34688f8536cca1504e
Nolan Fortin <widmo1414@gmail.com>
2021-11-23 19:38:07 -0500

    Scope Blur for all levels

--- Assets/Scripts/SnipingFunction/Snipe.cs
+++ Assets/Scripts/SnipingFunction/Snipe.cs
@@ -101,27 +101,27 @@ public class Snipe : MonoBehaviour
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            if (hit.collider != null && hit.collider.tag == "Enemy" || hit.collider.tag == "Window")
+            RaycastHit2D rayHit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (rayHit.collider != null && rayHit.collider.tag == "Enemy" || rayHit.collider.tag == "Window")
             {
                 this.GetComponentInParent<Snipe>().aimCheck = true;
-                if (hit.collider.tag == "Enemy")
+                if (rayHit.collider.tag == "Enemy")
                 {
-                    Pineapple = hit.collider.gameObject.GetComponent<PineappleAI>();
+                    Pineapple = rayHit.collider.gameObject.GetComponent<PineappleAI>();
                 }
-                else if (hit.collider.tag == "Window")
+                else if (rayHit.collider.tag == "Window")
                 {
-                    windowBreak = hit.collider.gameObject.GetComponent<WindowBreak>();
+                    windowBreak = rayHit.collider.gameObject.GetComponent<WindowBreak>();
                 }
             }
             else
             {
                 this.GetComponentInParent<Snipe>().aimCheck = false;
-                if (hit.collider.tag == "Enemy")
+                if (rayHit.collider.tag == "Enemy")
                 {
                     Pineapple = null;
                 }
-                else if (hit.collider.tag == "Window")
+                else if (rayHit.collider.tag == "Window")
                 {
                     windowBreak = null;
                 }
